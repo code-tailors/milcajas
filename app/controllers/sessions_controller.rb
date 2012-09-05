d847a2e5f682d757d6ff2a7bf1b91dc4ecefc132
blanchma <tute.unique@gmail.com>
2012-09-05 09:26:01 -0300

    Fix signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,6 @@ class SessionsController < ApplicationController
 
   def new
     dbsession = DropboxSession.new APP_KEY, APP_SECRET
-    dbsession.get_request_token
-    debugger
-    dbsession.get_access_token
     if dbsession.authorized?
       redirect_to refresh_dropbox_url, notice: "Signed in!"
     else
