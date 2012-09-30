9b7cb315cfc9772695087cf20c285c0352deec2b
blanchma <tute.unique@gmail.com>
2012-09-29 21:01:23 -0300

    Update db schema:

--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,11 @@ ActiveRecord::Schema.define(:version => 20120917184856) do
     t.string   "name"
     t.string   "path"
     t.string   "size"
+    t.string   "bytes"
     t.string   "mime_type"
     t.string   "description"
     t.string   "tags"
+    t.string   "category"
     t.string   "checksum"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
