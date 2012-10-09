63b07b59d73c5e9445c62d53c328b6146b9790a2
blanchma <tute.unique@gmail.com>
2012-10-09 13:54:34 -0300

    Improve UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find params[:id]
     @user.destroy
+    flash[:notice]="Usuario eliminado"
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    flash[:notice]="hola chabon como va"
   end
 end
